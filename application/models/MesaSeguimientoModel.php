ce91330ac7a5d136cd4d291b107cd4df127e6132
go1310 <diego.alvirde@gmail.com>
2017-12-13 10:57:17 -0600

    Mesa Seguimiento
    Se actualizo los nombres de las tablas a mayusculas

--- application/models/MesaSeguimientoModel.php
+++ application/models/MesaSeguimientoModel.php
@@ -4,25 +4,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class MesaSeguimientoModel extends CI_Model{
 
 	public function getGrupos(){
-		return $this->db->get('cat_grupo');
+		return $this->db->get('Cat_Grupo');
 	}
 
 	public function getColaboradores(){
-		return $this->db->get('cat_colaboradores');
+		return $this->db->get('Cat_Colaboradores');
 	}
 
 	public function validarMesaSeguimiento($id_colaborador){
 		$this->db->where("Id_Colaborador",$id_colaborador);
-		$query = $this->db->get('grupos_mesa_seguimiento');
+		$query = $this->db->get('Grupos_Mesa_Seguimiento');
 		return $query->result_array();
 	}
 
 	public function delConfigMesaSeguimiento($id){
-		return $this->db->delete('grupos_mesa_seguimiento', array('Id_Colaborador'=>$id));
+		return $this->db->delete('Grupos_Mesa_Seguimiento', array('Id_Colaborador'=>$id));
 	}
 
 	public function saveConfigMesaSeguimiento($data){
-		return $this->db->insert_batch('grupos_mesa_seguimiento', $data); 
+		return $this->db->insert_batch('Grupos_Mesa_Seguimiento', $data); 
 	}
 
 }
